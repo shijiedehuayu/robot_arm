b65904ff3626fe5df056db4d8b7d3c91d253adb4
shijiedehuayu <1163596672@qq.com>
2026-02-10 13:40:21 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    float b = 2.5;
+    char c = 'x';
+
+    cout << a << " " << b << " " << c <<endl;
+
+    return 0;
+}
